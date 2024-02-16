5a926c400fb84c2f7601734780286d8ea731a015
jahana <jahanakader915@gmail.com>
2024-02-16 10:51:08 +0530

    new model

---
+++ store/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=200,unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title=models.CharField(max_length=200)
+    price=models.PositiveIntegerField()
+    description=models.CharField(max_length=200)
+    picture=models.ImageField(upload_to="images")
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    is_active=models.BooleanField(default=True)
+    is_trending=models.BooleanField(default=False)
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.title 
+    
+
+class Basket(models.Model):
+    owner=models.OneToOneField(User,on_delete=models.CASCADE,related_name="cart")
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    is_active=models.BooleanField(default=True)
+
+
+class BasketItem(models.Model):
+    basket=models.ForeignKey(Basket,on_delete=models.CASCADE,related_name="cartitem")
+    Product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    is_active=models.BooleanField(default=True)
+
+
+
